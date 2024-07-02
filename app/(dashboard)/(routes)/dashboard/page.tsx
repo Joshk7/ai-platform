e18730e311d6e619a81c1a93d2a4fcd480af6b09
JoshK7 <joshk7683@gmail.com>
2024-07-02 16:18:29 -0500

    clerk authentication setup

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
   return (
-    <p>
-        Dashboard Page (protected)
-    </p>
+    <div>
+      <p>Dashboard Page (protected)</p>
+      <UserButton />
+    </div>
   );
 }
